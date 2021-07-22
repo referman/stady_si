c4866098341fbca11990e495fe30adf5c2eddc1f
referman <referman@mail.ru>
2021-07-22 16:28:35 +0300

    upd 16:28

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -19,36 +19,29 @@
 """
 
 ip_adress = input("Введите IP-адрес в формате 10.0.1.1: ")
-ip_chek = ''
-for i in ip_adress:
-   if i.isdigit():
-      ip_chek += i
-   elif i == '.':
-      ip_chek += i
-   else:
-      raise ValueError('Неправильный IP-адрес')
-print(ip_chek)
-         
-
-
-
-
-"""
-ip_adress_spl = ip_adress.split('.')
 try:
-   ip_adress_spl = [int(i) for i in ip_adress_spl]
-    i in ip_adress_spl:
-
-   if ip_adress_spl[0] >= 1 and ip_adress_spl[0] <=223:
-      print('unicast')
-   elif ip_adress_spl[0] >= 224 and ip_adress_spl[0] <=239:
-      print('multicast')
-   elif ip_adress == '255.255.255.255':
-      print('local broadcast')
-   elif ip_adress == '0.0.0.0':
-      print('unassigned')
-   else:
-      print('unused')
-except:
-   print('Неправильный IP-адрес')
-"""
+    ip_chek = ''
+    for i in ip_adress:
+        if i.isdigit():
+            ip_chek += i
+        elif i == '.':
+            ip_chek += i
+        else:
+            raise ValueError('Неправильный IP-адрес')
+    ip_adress_spl = ip_chek.split('.')
+    ip_adress_spl = [int(i) for i in ip_adress_spl]
+    for i in ip_adress_spl:
+        if i >= 256 or i <= -1:
+            raise ValueError('Неправильный IP-адрес')
+    if ip_adress_spl[0] >= 1 and ip_adress_spl[0] <=223:
+        print('unicast')
+    elif ip_adress_spl[0] >= 224 and ip_adress_spl[0] <=239:
+        print('multicast')
+    elif ip_adress == '255.255.255.255':
+        print('local broadcast')
+    elif ip_adress == '0.0.0.0':
+        print('unassigned')
+    else:
+        print('unused')
+except ValueError:
+    raise
