87625b2e83a69f770d4b734ff897bb3d148cb809
referman <referman@mail.ru>
2021-07-22 22:11:21 +0300

    upd 22:11

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,36 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+while True:
+    ip_adress = input("Введите IP-адрес в формате 10.0.1.1: ")
+    try:
+        ip_chek = ''
+        for i in ip_adress:
+            if i.isdigit():
+                ip_chek += i
+            elif i == '.':
+                ip_chek += i
+            else:
+                raise ValueError
+        ip_adress_spl = ip_chek.split('.')
+        ip_adress_spl = [int(i) for i in ip_adress_spl]
+        ip_adress_spl_len = len(ip_adress_spl)
+        if ip_adress_spl_len <= 3 or ip_adress_spl_len > 4:
+            raise ValueError
+        for i in ip_adress_spl:
+            if i >= 256 or i <= -1:
+                raise ValueError
+        if ip_adress_spl[0] in range(1, 224):
+            print('unicast')
+        elif ip_adress_spl[0] in range(224, 240):
+            print('multicast')
+        elif ip_adress == '255.255.255.255':
+            print('local broadcast')
+        elif ip_adress == '0.0.0.0':
+            print('unassigned')
+        else:
+            print('unused')
+    except ValueError:
+        print('Неправильный IP-адрес')
+    else:
+        break
