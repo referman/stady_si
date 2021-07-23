58586cbaef4b21324fa0a3b9263da726ec775fa2
referman <referman@mail.ru>
2021-07-23 15:31:22 +0300

    upd 15:31

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,12 +14,12 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
-result = {}
+result = []
 with open('ospf.txt') as f:
     for i in f:
         i = i.split()
-        print(i)
-        for a in i:
-            result['Prefix'] = a
+        result.append({'Prefix': i[1], 'AD/Metric': i[2].strip('[]'), 'Next-Hop': i[4].strip(','), 'Last update': i[5].strip(','), 'Outbound Interface': i[6]})
 
-print(result)
+for i in result:
+    for a, b in i.items():
+        print('{:25}{}'.format(a, b))
