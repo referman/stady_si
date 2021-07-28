91b37a1c4b213bb451fae89f35a66703660f2169
referman <referman@mail.ru>
2021-07-28 18:18:34 +0300

    upd 18:18

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,16 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+import subprocess
+def ping_ip_addresses(adress):
+    for i in adress:
+        ok_1 = []
+        not_01 = []
+        reply = subprocess.run(['ping', '-c', '3', '-n', i], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+        if reply.returncode == 0:
+            ok_1.append(i)
+        else:
+            not_01.append(i)
+    kort = (ok_1, not_01)
+
+    return kort
