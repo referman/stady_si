99f051d283908b3ade1efd506d83594419f7ca00
referman <referman@mail.ru>
2021-07-31 22:45:36 +0300

    upd 22:45

--- exercises/15_module_re/answer_task_15_1a.py
+++ exercises/15_module_re/answer_task_15_1a.py
@@ -24,37 +24,8 @@
 а не ввод пользователя.
 
 """
-
-#Моя сложная версия
-"""
 import re
-def get_ip_from_cfg(file):
-    spisok = []
-    with open(file) as f:
-        asl = f.read()
-        spisok = re.split(r'[!\n]+', asl)
-        yet = ('interface', ' ip address')
-        spisok_1 = [i for i in spisok if i.startswith(yet)]
-        spisok_1 = ' '.join(spisok_1)
-        regular = (r'(interface \S+\s+ip address (\S+) (\S+))')
-        spisok_2 = re.finditer(regular, spisok_1)
-        spisok_3 = []
-        for i in spisok_2:
-            spisok_3.append(i.group())
-        spisok_3 = ''.join(spisok_1)
-        regular_2 = r'((interface) (\S+)\s+(ip address \S+ \S+))'
-        spisok_4 = [i.group(1) for i in re.finditer(regular_2, spisok_3)]
-        spisok_4 = ' '.join(spisok_4)
-        print(spisok_4)
-        regular_3 = (r'ip address (\S+) (\S+)')
-        regular_4 = r'(interface) (\S+)\s+(ip address \S+ \S+)'
-        spisok_5 = [b.group(2) for b in re.finditer(regular_4, spisok_4)]
-        spisok_6 = [i.groups() for i in re.finditer(regular_3, spisok_4)]
-        slovar = dict(zip(spisok_5, spisok_6))
-        return slovar
-"""
 
-import re
 def get_ip_from_cfg(config):
     with open(config) as f:
         regex = re.compile(
@@ -65,7 +36,4 @@ def get_ip_from_cfg(config):
         match = regex.finditer(f.read())
 
     result = {m.group("intf"): m.group("ip", "mask") for m in match}
-    return print(result)
-
-get_ip_from_cfg('config_r1.txt')
-
+    return result
